3e5e8127c7de20e8b30bb27edd528ef26e8c41e9
W508636_wexinc <yanni.sun@wexinc.com>
2026-08-07 17:12:10 +1000

    Restore Program.cs to previous version with direct service calls

--- Program.cs
+++ Program.cs
@@ -1,24 +1,31 @@
-﻿using Microsoft.Extensions.Configuration;
-using StockCli.Data;
+﻿using StockCli.Data;
 using StockCli.Models;
 using StockCli.Repositories;
 using StockCli.Services;
 
-var configuration = new ConfigurationBuilder()
-    .SetBasePath(AppContext.BaseDirectory)
-    .AddJsonFile("appsettings.json", optional: false)
-    .Build();
+string connectionString =
+    "Server=.;Database=StockManagementDb;Trusted_Connection=True;TrustServerCertificate=True;";
 
-var connectionString = configuration.GetConnectionString("StockDb")
-    ?? throw new InvalidOperationException("Connection string 'StockDb' not found.");
-
+//database
 var db = new StockDb(connectionString);
-var repository = new StockRepository(db);
-var service = new StockService(repository);
 
-var products = service.GetAllProducts();
+//Repos
+var Repository = new StockRepository(db);
+
+//service
+var Service = new StockService(Repository);
+
+//test
+Service.AddProduct(new StockItem
+{
+    Name = "Orange",
+    Quantity = 5,
+    Price = 4.50m
+});
+var products = Service.GetAllProducts();
 
 foreach (var product in products)
 {
-    Console.WriteLine($"{product.Id} | {product.Name} | {product.Quantity} | {product.Price:F2}");
+    Console.WriteLine(
+        $"{product.Id}{product.Name}{product.Quantity}");
 }
